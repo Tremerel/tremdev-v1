cfa564304d9bf31d5b79faf263aeb396acc5e4a4
Tremerel <84023901+Tremerel@users.noreply.github.com>
2022-01-15 21:11:51 +0100

    adding navbar

--- app.js
+++ app.js
@@ -4,13 +4,28 @@ AOS.init({
     offset: 120,
 });
 
-function toggleMenu(){
-    const menuToggle = document.querySelector('.toggle');
-    // const nav = document.querySelector('nav');
+// function toggleMenu(){
+//     const menuToggle = document.querySelector('.toggle');
+//     // const nav = document.querySelector('nav');
 
-    menuToggle.classList.toggle('active');
-    // nav.classList.toggle('active');
-}
+//     menuToggle.classList.toggle('active');
+//     // nav.classList.toggle('active');
+// }
+
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if(visibility === "false") {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+})
 
 const loader = document.querySelector('.loader');
 window.addEventListener('load', () => {
